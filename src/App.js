0476cb900042896b74c7ce535e5683bacc950617
fujiansheng <15221448373@163.com>
2017-10-30 20:32:43 +0800

    init

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
         <ul>
           <li><Link to={`/`}>blank</Link></li>
           <li><Link to={`/todo-app`}>todo-app</Link></li>
+          <li><Link to={`/home`}>home</Link></li>
         </ul>
         {this.props.children}
       </div>
