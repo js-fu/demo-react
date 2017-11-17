394c258a870c16e57a9770b28fe8c29113fa08d7
fujiansheng <15221448373@163.com>
2017-11-17 17:09:24 +0800

    modify code splitting

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-import todoAppAction from './todoAppAction';
-
-export {
-  todoAppAction,
-};
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import todoAppReducer from './todoAppReducer';
-
-let rootReducers = combineReducers({
-  todoAppReducer,
-});
-
-export default rootReducers;
